30a2d4aeee6f7c93993eeacbb74aad174b966356
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-06-06 10:05:21 +0200

    useQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,62 @@
+import { gql, useQuery } from "@apollo/client";
+
 function App() {
   return (
     <div className="App">
-      <Quote text="bla bla" author="Blablator" />
+      <RandomQuote />
     </div>
   );
 }
 
 export default App;
 
-function Quote({ text, author }) {
+const RANDOM_QUOTE_QUERY = gql`
+  query getRandomQuote {
+    randomQuote {
+      text
+      author
+    }
+    greetingCat
+  }
+`;
+
+function RandomQuote(params) {
+  const { data, loading, error, refetch } = useQuery(RANDOM_QUOTE_QUERY, {
+    onError: (error) => {
+      console.log("error", error);
+    },
+    errorPolicy: "all",
+  });
+
+  if (loading) {
+    return "Quote is loading...";
+  }
+  if (error) {
+    return "Could not load quote!";
+  }
+  const { text, author } = data.randomQuote;
+  const cat = data.greetingCat;
+
+  return (
+    <>
+      <Quote text={text} author={author} cat={cat} />
+      <button
+        onClick={() => {
+          refetch();
+        }}
+      >
+        Get quote
+      </button>
+    </>
+  );
+}
+
+function Quote({ text, author, cat }) {
   return (
     <div>
       <div>{text}</div>
       <div>{author}</div>
+      <div>{cat}</div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloProvider,
+} from "@apollo/client";
+
+const URL = "https://young-savannah-06405.herokuapp.com/";
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: URL,
+  }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
