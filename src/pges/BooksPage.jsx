eeee810a4396c618bc8a19234d3271333584968c
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2022-06-14 09:04:36 +0200

    useParams,useNavigate,onSearchQueryChange

--- src/pges/BooksPage.jsx
+++ src/pges/BooksPage.jsx
@@ -1,11 +1,14 @@
 import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router";
 import Book, { BOOK_FIELDS_FRAGMENT } from "../conponents/Book/Book";
 import Link from "../conponents/Link";
+import SearchBox from "./SearchBox";
+import Box from "@mui/material/Box";
+import { useNavigate } from "react-router-dom";
 
 export const GET_ALL_BOOKS_QUERY = gql`
-  query GetBooks {
-    books {
-      __typename
+  query GetBooks($searchQuery: String!) {
+    books(searchQuery: $searchQuery) {
       ...bookFields
     }
   }
@@ -13,7 +16,11 @@ export const GET_ALL_BOOKS_QUERY = gql`
 `;
 
 const BooksPage = () => {
-  const { loading, error, data } = useQuery(GET_ALL_BOOKS_QUERY);
+  const navigate = useNavigate();
+  const { searchQuery = "" } = useParams();
+  const { loading, error, data } = useQuery(GET_ALL_BOOKS_QUERY, {
+    variables: { searchQuery },
+  });
   if (loading) {
     return <p>Loading...</p>;
   }
@@ -21,19 +28,40 @@ const BooksPage = () => {
     return <p>Could not load books</p>;
   }
   const { books } = data;
+  const hasBooks = books.length > 0;
 
   return (
     <>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        books.map((book) => (
-          <Link key={book.id} to={`/books/${book.id}`}>
-            <Book {...book} />
-          </Link>
-        ))
-      )}
-      ;
+      <div>
+        <SearchBox
+          searchQuery={searchQuery}
+          onSearchQueryChange={(newSearchQuery) =>
+            navigate(`/books/search/${encodeURIComponent(newSearchQuery)}`)
+          }
+        />
+      </div>
+      <Box
+        sx={{
+          textAlign: "center",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        {loading ? (
+          <p>Loading...</p>
+        ) : hasBooks ? (
+          books.map((book) => (
+            <Link key={book.id} to={`/books/${book.id}`}>
+              <Book {...book} />
+            </Link>
+          ))
+        ) : (
+          <p>Books no found</p>
+        )}
+      </Box>
     </>
   );
 };
